e718bae95106f6c638afbc9868ac81a4f151954d
Riccardo Zese <riccardo.zese@unife.it>
2023-11-23 11:33:07 +0100

    fix in time collection

--- paper-test/collect_times.py
+++ paper-test/collect_times.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
 import sys
+import numpy
 
 namein=str(sys.argv[1])
 
-tt=0
+tt=[]
 ttp=0
 ttpb=0
-ct=0
+max = 0
+min = 1000000
+ct = 0
 ctp=0
 ctpb=0
 sec=0
 prob=-1
 
-
 with open(namein, "r") as f:
 	lines = f.readlines()
 	for line in lines:
 		if "Time =" in line:
-			t = line.split()[2][:-1]
-			tt = tt + float(t)
+			t = float(line.split()[2][:-1])
+			tt.append(t)
 			ct = ct +1
+			if t < min:
+				min = t
+			if t > max:
+				max = t
 		elif "Prob =" in line:
 			p = line.split()[2][:-1]
 			if prob==-1:
@@ -28,9 +34,13 @@ with open(namein, "r") as f:
 			elif prob!=float(p):
 				print("Error in prob, should be" + str(prob) + ", it is " + p)
 		elif "Execution" in line:
-			t = line.split()[3][:-1]
-			tt = tt + float(t)
+			t = float(line.split()[3][:-1])
+			tt.append(t)
 			ct = ct +1
+			if t < min:
+				min = t
+			if t > max:
+				max = t
 		elif "Probability" in line:
 			p = line.split()[4][:-1]
 			if prob==-1:
@@ -39,8 +49,7 @@ with open(namein, "r") as f:
 				print("Error in prob, should be" + str(prob) + ", it is " + p)
 
 f.close()
-tta = 0
-if ct > 0:
-	tta=tt/ct
+tta = numpy.average(tt)
+ttstd = numpy.std(tt)
 
-print("time: " + str(tta) + " on " + str(ct) + " Prob: " + str(prob))
+print("time: " + str(tta) + " pm " + str(ttstd) + " on " + str(ct) + " Prob: " + str(prob) + " max-min " + str(max) + " " + str(min))
--- paper-test/create_csv.py
+++ paper-test/create_csv.py
@@ -10,15 +10,11 @@ out = open(sys.argv[2],"w")
 
 with open(namein, "r") as f:
 	lines = f.readlines()
-	
+
+	out.write("i;time;std;max;min\n")
 	for i in range(2,11):
-		out.write(";" + str(i))
-	out.write("\n")
-	
-	for j in range(2,11):
-		out.write(";" + lines[idx].split()[1])
-		idx = idx + 2
-	out.write("\n")
+		# "time: " + str(tta) + " pm " + str(ttstd) + " on " + str(ct) + " Prob: " + str(prob) + " max-min " + str(max) + " " + str(min)
+		out.write(str(i) + ";" + lines[idx].split()[1] + ";" + lines[idx].split()[3] + ";" + lines[idx].split()[9] + ";" + lines[idx].split()[10] + "\n")
 			
 f.close()
 
